b6a54ca0dded8c7a08bbf3e88788060b7e3420d3
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-23 14:27:34 +0530

    db connection + server listening done!

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { MONGODB_URI , PORT } = require('./utils/config');
+const app = require('./app')
+
+
+console.log(`Connecting to the database...`);
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log(`connected to the database!`);
+
+        // Start the server 
+        app.listen(PORT, () => {
+            console.log(`Server running @ http://localhost:${PORT}`);
+    
+        });
+    })
+    
+    
+    .catch((error) => {
+    console.log(`Error connecting to the database: ${error}`)
+})
